e17bad165465aa90b87a43f2789d5df24d47ae41
Endrit Kastrati <endritkastrati8@gmail.com>
2024-03-05 01:15:12 +0100

    Adding Atbash Cipher logic.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ function atbashCipher(text) {
 let result = "";
     for (let i = 0; i < text.length; i++) {
         let charCode = text.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) { // Uppercase letters
-            result += String.fromCharCode(90 - (charCode - 65)); // Convert to Atbash cipher
-        } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
-            result += String.fromCharCode(122 - (charCode - 97)); // Convert to Atbash cipher
+        if (charCode >= 65 && charCode <= 90) { // Uppercase 
+            result += String.fromCharCode(90 - (charCode - 65)); // Konvertimi ne Atbash cipher
+        } else if (charCode >= 97 && charCode <= 122) { // Lowercase
+            result += String.fromCharCode(122 - (charCode - 97)); // Konvertimi ne Atbash cipher
         } else {
-            result += text.charAt(i); // Non-alphabetic characters remain unchanged
+            result += text.charAt(i); // Simbolet qe nuk jane letra mbesin nisoj
         }
     }
     return result;
